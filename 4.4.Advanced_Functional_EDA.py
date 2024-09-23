15a819d2d6f0bb5d4e97cf2feca83987008304de
Hale <itshalenurgurpinar@gmail.com>
2024-09-23 17:56:13 +0300

    4.4. Advanced Functional EDA -- Analysis of Categorical Variables and Analysis of Numerical Variables

--- 4.4.Advanced_Functional_EDA.py
+++ 4.4.Advanced_Functional_EDA.py
@@ -1,6 +1,7 @@
 import seaborn as sns
 import pandas as pd
 import numpy as np
+from matplotlib import pyplot as plt
 
 pd.set_option('display.max_columns', None)  # Get rid of 3 dots(...). Generally not prefer in dataset has many columns.
 pd.set_option('display.width', 500)  # Table ***
@@ -74,10 +75,83 @@ df['survived'].value_counts()
 def cat_summary(dataframe, col_name):
     print(pd.DataFrame({col_name: dataframe[col_name].value_counts(),
                         "Ratio": 100 * dataframe[col_name].value_counts() / len(dataframe)}))
+    print("################################################################################")
 
 
 print(df, "sex")
 
 for col in cat_cols:
     cat_summary(df, col)  # all categorical variables in dataframe
+
+
+def cat_summary(dataframe, col_name, plot=False):
+    print(pd.DataFrame({col_name: dataframe[col_name].value_counts(),
+                        "Ratio": 100 * dataframe[col_name].value_counts() / len(dataframe)}))
+    print("################################################################################")
+
+    if plot:
+        sns.countplot(x=dataframe[col_name], data=dataframe)
+        plt.show(block=True)
+
+
+cat_summary(df, "sex", plot=True)
+
+for col in cat_cols:
+    cat_summary(df, col, plot=True)  # all categorical variables graphics and info in dataframe
+
+
+df["adult_male"].astype(int)  # True=1, False=0  convert t-f to num
+
+for col in cat_cols:
+    if df[col].dtypes == "bool":
+        df[col] = df[col].astype(int)
+        cat_summary(df, col, plot=True)
+    else:
+        cat_summary(df, col, plot=True)
+
+
+# Analysis of Numerical Variables
+import seaborn as sns
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+
+pd.set_option('display.max_columns', None)  # Get rid of 3 dots(...). Generally not prefer in dataset has many columns.
+pd.set_option('display.width', 500)  # Table ***
+df = sns.load_dataset("titanic")
+
+print(df[["age", "fare"]].describe().T)
+
+print([col for col in df.columns if df[col].dtypes in ["int64", "float64"]])  # get numerical var.
+
+num_cols = [col for col in df.columns if df[col].dtypes in ["int64", "float64"]]
+num_cols = [col for col in df.columns if col not in cat_cols]
+
+
+def num_summary(dataframe, numerical_col):
+    quantiles = [0, 0.05, 0.5, 0.7,  0.95, 0.99, 1]
+    print(dataframe[numerical_col].describe(quantiles).T)
+
+
+num_summary(df, "age")
+
+for col in num_cols:       # all categorical variables info in dataframe
+    num_summary(df, col)
+
+
+def num_summary(dataframe, numerical_col, plot=False):
+    quantiles = [0, 0.05, 0.5, 0.7, 0.95, 0.99, 1]
+    print(dataframe[numerical_col].describe(quantiles).T)
+
+    if plot:  # if plot equals to true continue next step
+        dataframe[numerical_col].hist()
+        plt.xlabel(numerical_col)
+        plt.title(numerical_col)
+        plt.show(block=True)
+
+
+num_summary(df, "age", plot=True)  # age histogram graph
+
+for col in num_cols:
+    num_summary(df, col, plot=True)   # all numerical variables histogram graphics
 
