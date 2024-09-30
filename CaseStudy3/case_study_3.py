4141be2f44bce756bbefedcda93c36292e8c8d52
Hale <itshalenurgurpinar@gmail.com>
2024-09-30 14:19:47 +0300

    Case Study 3

---
+++ CaseStudy3/case_study_3.py
@@ -0,0 +1,8 @@
+# ##############################################
+# Gezinomi Kural Tabanlı Sınıflandırma ile Potansiyel Müşteri Getirisi Hesaplama
+# (Calculating Potential Customer Return with Gezinomi Rule Based Classification)
+# #############################################
+
+# ##############################################
+# 1:
+# ##############################################
